3e3524d29acbd9682d9656fd2eb70ece77006a56
ahmadbudairi <1210734@student.birzeit.edu>
2024-06-01 23:03:30 +0300

    first

---
+++ app/src/main/java/com/example/hello/Main.java
@@ -0,0 +1,96 @@
+package com.example.hello;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class Main extends AppCompatActivity {
+    private RequestQueue queue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText username = findViewById(R.id.usernameText);
+        final EditText password = findViewById(R.id.passwordText);
+
+        TextView textViewSignUp = findViewById(R.id.textViewSignUp);
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                canLogin(user, pass);
+            }
+        });
+
+    }
+
+    private void canLogin(String username, String pass) {
+        String url = "http://10.0.2.2/android/canLoggin.php?username=" + username + "&pass=" + pass;
+        Log.d("url", url);
+        queue = Volley.newRequestQueue(this);
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Response", response.toString());
+                        String check = "";
+                        try {
+                            check= response.getString("answer");
+                        } catch (JSONException exception) {
+                            Log.d("Error", exception.toString());
+                        }
+                        if(check.equals("yes")){
+                            Intent intent = new Intent(Main.this, MainMenu.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(Main.this, "Username or password is incorrect", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("VolleyError", error.toString());
+            }
+        });
+        queue.add(request);
+    }
+
+}
---
+++ app/src/main/java/com/example/hello/RegisterActivity.java
@@ -0,0 +1,145 @@
+package com.example.hello;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText username = findViewById(R.id.usernameText);
+        final EditText password = findViewById(R.id.passwordText);
+        final EditText email = findViewById(R.id.emailText);
+
+        TextView textViewLogIn = findViewById(R.id.textViewLogIn);
+        textViewLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, Main.class);
+                startActivity(intent);
+            }
+        });
+
+        Button register = findViewById(R.id.registerButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()){
+                    String user = username.getText().toString();
+                    String pass = password.getText().toString();
+                    String EMAIL = email.getText().toString();
+                    registerNew(user,pass,EMAIL);
+                }else{
+                    email.setBackgroundColor(Color.RED);
+                    Toast.makeText(RegisterActivity.this, "Email not valid", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void registerNew(String username, String pass,String email){
+        String url = "http://10.0.2.2/android/registerNew.php";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("username", username);
+            jsonParams.put("pass", pass);
+            jsonParams.put("email", email);
+            jsonParams.put("permission", "customer");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+        // Create a JsonObjectRequest with POST method
+        StringRequest request = new StringRequest(Request.Method.POST, url, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Response", "RESPONSE IS " + response);
+                String str = "" ;
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    // on below line we are displaying a success toast message.
+                    str = jsonObject.getString("answer");
+                    Log.d("Response2", "RESPONSE2 IS " + str);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if(str.equals("error")){
+                    Toast.makeText(RegisterActivity.this, "Username or email is already in use", Toast.LENGTH_LONG).show();
+                }else{
+                    Intent intent = new Intent(RegisterActivity.this, Main.class);
+                    startActivity(intent);
+                }
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // method to handle errors.
+                Toast.makeText(RegisterActivity.this,
+                        "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                // as we are passing data in the form of url encoded
+                // so we are passing the content type below
+                return "application/x-www-form-urlencoded; charset=UTF-8";
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+
+                // below line we are creating a map for storing
+                // our values in key and value pair.
+                Map<String, String> params = new HashMap<String, String>();
+
+                // on below line we are passing our
+                // key and value pair to our parameters.
+                params.put("username", username);
+                params.put("pass", pass);
+                params.put("email", email);
+                params.put("permission", "customer");
+
+                // at last we are returning our params.
+                return params;
+            }
+        };
+        queue.add(request);
+    }
+
+}
