80d6955821e48ecf593ddb6b8d43bbac506176c4
SoraFujin <ahmaddiab29072002@gmail.com>
2024-06-06 03:01:20 +0300

    error

--- app/src/main/java/com/example/hello/Menu.java
+++ app/src/main/java/com/example/hello/Menu.java
@@ -1,116 +1,47 @@
 package com.example.hello;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.example.hello.PopularPlace;
+import com.example.hello.PopularPlaceAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Menu extends AppCompatActivity {
 
-    private List<TripPlaces> items = new ArrayList<>();
-    private RecyclerView recycler;
+    private RecyclerView popularPlacesRecyclerView;
+    private PopularPlaceAdapter popularPlaceAdapter;
+    private List<PopularPlace> popularPlaces;
+
+    // TODO LIST:
+    // Dont let the user put the app in horizontal mode
+    // Remove exported = true from activity_main
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        recycler = findViewById(R.id.main_menu);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recycler.setLayoutManager(layoutManager);
-        setSpinner();
-        loadItems();
-    }
+        popularPlacesRecyclerView = findViewById(R.id.popular_places_recycler_view);
+        popularPlacesRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
-    private void setSpinner() {
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.options_array, R.layout.spinner_item_layout);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        popularPlaces = new ArrayList<>();
+        //TODO GET THIS DATA FROM PHP BACKEND
+        popularPlaces.add(new PopularPlace("Palestine, Jerusalem", R.drawable.jerusalem));
+        popularPlaces.add(new PopularPlace("Palestine, Bethlehem", R.drawable.bethlehem));
+        popularPlaces.add(new PopularPlace("Palestine, Jerusalem", R.drawable.jerusalem));
+        popularPlaces.add(new PopularPlace("Palestine, Bethlehem", R.drawable.bethlehem));
+        popularPlaces.add(new PopularPlace("Palestine, Jerusalem", R.drawable.jerusalem));
+        popularPlaces.add(new PopularPlace("Palestine, Bethlehem", R.drawable.bethlehem));
 
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // Redirect to selected activity from dropdown menu
-                String selectedOption = parent.getItemAtPosition(position).toString();
-                switch (selectedOption) {
-
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-    }
-
-    private void loadItems() {
-        String baseURL = "http://10.0.2.2/android/getPlaces.php";
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, baseURL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-
-
-                        try {
-
-                            JSONArray array = new JSONArray(response);
-                            for (int i = 0; i<array.length(); i++){
-
-                                JSONObject object = array.getJSONObject(i);
-
-                                String name = object.getString("name");
-                                String image = object.getString("imageURL");
-
-                                Log.d("image", image);
-
-                                TripPlaces tripPlaces = new TripPlaces(name, image);
-                                items.add(tripPlaces);
-                            }
-
-                        }catch (Exception e){
-
-                        }
-
-                        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(Menu.this,
-                                items);
-                        recycler.setAdapter(adapter);
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-
-                Toast.makeText(Menu.this, error.toString(),Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-        Volley.newRequestQueue(Menu.this).add(stringRequest);
-
+        popularPlaceAdapter = new PopularPlaceAdapter(popularPlaces);
+        popularPlacesRecyclerView.setAdapter(popularPlaceAdapter);
     }
 
 }
