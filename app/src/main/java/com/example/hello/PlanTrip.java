c5d37c9f021f55518a74f50537fb84e14ddec143
SoraFujin <ahmaddiab29072002@gmail.com>
2024-06-07 04:16:46 +0300

    Fragments

--- app/src/main/java/com/example/hello/PlanTrip.java
+++ app/src/main/java/com/example/hello/PlanTrip.java
@@ -1,14 +1,77 @@
 package com.example.hello;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class PlanTrip extends AppCompatActivity {
+    private ImageView landImageView, hotelImageView, resturantImageView, transportationImageView;
+    private  FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_trip);
+
+        landImageView = findViewById(R.id.land_mark_icon);
+        hotelImageView = findViewById(R.id.hotel_icon);
+        resturantImageView = findViewById(R.id.food_icon);
+        transportationImageView = findViewById(R.id.transport_icon);
+
+        landImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.landMarkfragmentContainer, HotelFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+
+            }
+        });
+
+
+        hotelImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.landMarkfragmentContainer, TransportFragment.class, null).setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+
+            }
+        });
+
+        resturantImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.landMarkfragmentContainer, LandMarkFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+
+            }
+        });
+
+        transportationImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.landMarkfragmentContainer, FoodFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+
+            }
+        });
+
     }
+
+
+
+
 }
